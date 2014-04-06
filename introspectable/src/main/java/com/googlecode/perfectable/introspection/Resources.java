dad526dea86619b6204d7b898e811723c910c74c
nivertius <nivertius@gmail.com>
2014-04-06 19:52:18 +0200

    checks for resources

--- introspectable/src/main/java/com/googlecode/perfectable/introspection/Resources.java
+++ introspectable/src/main/java/com/googlecode/perfectable/introspection/Resources.java
@@ -2,8 +2,11 @@ package com.googlecode.perfectable.introspection;
 
 import java.io.InputStream;
 
+import javax.annotation.Nullable;
+
 public final class Resources {
 
+	@Nullable
 	public static InputStream load(String path) {
 		return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
 	}
