b94bb9663e01a9d6663f198e4661f4fb527c1ccb
Paweł Płazieński <nivertius@gmail.com>
2015-11-19 19:12:37 +0100

    use ProxyBuilder for NotifyingVisualProxy

--- introspectable/src/main/java/com/googlecode/perfectable/introspection/proxy/Invocation.java
+++ introspectable/src/main/java/com/googlecode/perfectable/introspection/proxy/Invocation.java
@@ -2,24 +2,27 @@ package com.googlecode.perfectable.introspection.proxy;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Optional;
+
+import com.googlecode.perfectable.introspection.Methods;
 
 public final class Invocation<T> {
-
+	
 	private final Method method;
-	private final Object[] args;
-
-	public Invocation(Method method, Object[] args) {
+	private final Object[] arguments;
+	
+	public Invocation(Method method, Object[] arguments) {
 		this.method = method;
-		this.args = args;
-	}
-
-	public static Invocation<?> of(Method method, Object[] args) {
-		return new Invocation<>(method, args);
+		this.arguments = arguments;
 	}
 	
+	public static Invocation<?> of(Method method, Object[] arguments) {
+		return new Invocation<>(method, arguments);
+	}
+
 	public Object invokeOn(T receiver) throws Throwable {
 		try {
-			Object result = this.method.invoke(receiver, this.args);
+			Object result = this.method.invoke(receiver, this.arguments);
 			return result;
 		}
 		catch(InvocationTargetException e) {
@@ -30,4 +33,18 @@ public final class Invocation<T> {
 		}
 	}
 	
+	public <X> Invocation<X> asSimilarTo(X similarInstance) throws NoSuchMethodException {
+		@SuppressWarnings("unchecked")
+		Class<X> similarClass = (Class<X>) similarInstance.getClass();
+		return asSimilar(similarClass);
+	}
+	
+	public <X> Invocation<X> asSimilar(Class<X> similarClass) throws NoSuchMethodException {
+		Optional<Method> similarOption = Methods.similar(similarClass, this.method);
+		Method similar = similarOption.orElseThrow(NoSuchMethodException::new);
+		@SuppressWarnings("unchecked")
+		Invocation<X> checked = (Invocation<X>) Invocation.of(similar, this.arguments);
+		return checked;
+	}
+
 }
