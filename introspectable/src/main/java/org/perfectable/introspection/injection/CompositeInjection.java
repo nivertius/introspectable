306cd38b2ad35cf4a723df9e84e87ad1aaf5b769
Paweł Płazieński <nivertius@gmail.com>
2016-11-11 22:27:03 +0100

    Make some hard to infer type parameters explicit

--- introspectable/src/main/java/org/perfectable/introspection/injection/CompositeInjection.java
+++ introspectable/src/main/java/org/perfectable/introspection/injection/CompositeInjection.java
@@ -3,15 +3,15 @@ package org.perfectable.introspection.injection;
 import com.google.common.collect.ImmutableList;
 
 public final class CompositeInjection<T> implements Injection<T> {
-	private final ImmutableList<Injection<? super T>> components;
+	private final ImmutableList<? extends Injection<? super T>> components;
 
 	@SafeVarargs
 	@SuppressWarnings("varargs")
 	public static <TX> CompositeInjection<TX> create(Injection<? super TX>... injections) {
-		return new CompositeInjection<>(ImmutableList.copyOf(injections));
+		return new CompositeInjection<TX>(ImmutableList.copyOf(injections));
 	}
 
-	private CompositeInjection(ImmutableList<Injection<? super T>> injections) {
+	private CompositeInjection(ImmutableList<? extends Injection<? super T>> injections) {
 		this.components = injections;
 	}
 
