69cc58dc4cb9361bdb519e5f128e0f385149f047
Paweł Płazieński <nivertius@gmail.com>
2016-12-25 14:35:41 +0100

    Extract AbstractQuery for iterable queries

---
+++ src/main/java/org/perfectable/introspection/query/AbstractQuery.java
@@ -0,0 +1,36 @@
+package org.perfectable.introspection.query;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import com.google.common.collect.Iterators;
+
+public abstract class AbstractQuery<E, Q extends AbstractQuery<E, ? extends Q>> implements Iterable<E> {
+
+	public abstract Q matching(Predicate<? super E> filter);
+
+	public abstract Stream<E> stream();
+
+	@Override
+	public Iterator<E> iterator() {
+		return stream().iterator();
+	}
+
+	public final E single() {
+		return Iterators.getOnlyElement(iterator());
+	}
+
+	public final Optional<E> option() {
+		Iterator<E> iterator = iterator();
+		if (iterator.hasNext()) {
+			return Optional.of(iterator.next());
+		}
+		return Optional.empty();
+	}
+
+	AbstractQuery() {
+		// package extension only
+	}
+}
