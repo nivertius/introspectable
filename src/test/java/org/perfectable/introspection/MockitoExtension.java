7031f3b50217738e1f7be9b128b7e42c5e527ec7
Paweł Płazieński <nivertius@gmail.com>
2017-02-16 19:11:21 +0100

    Mark nullability in tests

--- src/test/java/org/perfectable/introspection/MockitoExtension.java
+++ src/test/java/org/perfectable/introspection/MockitoExtension.java
@@ -1,5 +1,7 @@
 package org.perfectable.introspection;
 
+import javax.annotation.Nullable;
+
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExtensionContext;
@@ -10,12 +12,12 @@ import org.mockito.MockitoAnnotations;
 // adding testable would make circular dependency and this is really simple extension which should be in mockito
 public class MockitoExtension implements TestInstancePostProcessor, AfterEachCallback {
 	@Override
-	public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
+	public void postProcessTestInstance(@Nullable Object testInstance, @Nullable ExtensionContext context) {
 		MockitoAnnotations.initMocks(testInstance);
 	}
 
 	@Override
-	public void afterEach(TestExtensionContext context) {
+	public void afterEach(@Nullable TestExtensionContext context) {
 		Mockito.validateMockitoUsage();
 	}
 
