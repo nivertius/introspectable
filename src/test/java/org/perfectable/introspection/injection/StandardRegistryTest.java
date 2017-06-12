d2cd142a3a345a614867761def36fa548aa0c979
Paweł Płazieński <nivertius@gmail.com>
2017-06-12 13:01:15 +0200

    Increase injection test coverage

--- src/test/java/org/perfectable/introspection/injection/StandardRegistryTest.java
+++ src/test/java/org/perfectable/introspection/injection/StandardRegistryTest.java
@@ -1,6 +1,9 @@
 package org.perfectable.introspection.injection;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import javax.inject.Inject;
+import javax.inject.Qualifier;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +23,7 @@ public class StandardRegistryTest {
 	}
 
 	@Test
-	public void testConstructorSimple() {
+	public void testConstructorCreated() {
 		StandardRegistry registry = StandardRegistry.create();
 		registry.register(EmptyService.class);
 		registry.register(ConstructorService.class);
@@ -32,7 +35,78 @@ public class StandardRegistryTest {
 		service.assertAnythingInjected();
 	}
 
+	@Test
+	public void testConstructorProvided() {
+		StandardRegistry registry = StandardRegistry.create();
+		EmptyService emptyService = new EmptyService();
+		registry.register(emptyService);
+		registry.register(ConstructorService.class);
+
+		ConstructorService service = registry.fetch(typed(ConstructorService.class));
+
+		assertThat(service)
+			.isNotNull();
+		service.assertInjected(emptyService);
+	}
+
+	@Test
+	public void testQualifiedQueryConstruction() {
+		StandardRegistry registry = StandardRegistry.create();
+		registry.register(EmptyService.class);
+		registry.register(QualifiedService.class);
+
+		EmptyService service =
+			registry.fetch(typed(EmptyService.class).qualifiedWith(TestQualifier.class));
+
+		assertThat(service)
+			.isNotNull()
+			.isInstanceOf(QualifiedService.class);
+	}
+
+	@Test
+	public void testQualifiedQuerySingleton() {
+		StandardRegistry registry = StandardRegistry.create();
+		EmptyService unqualifiedEmptyService = new EmptyService();
+		EmptyService qualifiedEmptyService = new QualifiedService();
+		registry.register(unqualifiedEmptyService);
+		registry.register(qualifiedEmptyService);
+
+		EmptyService service =
+			registry.fetch(typed(EmptyService.class).qualifiedWith(TestQualifier.class));
+
+		assertThat(service)
+			.isSameAs(qualifiedEmptyService);
+	}
+
+	@Test
+	public void testQualifiedSetterProvided() {
+		StandardRegistry registry = StandardRegistry.create();
+		EmptyService unqualifiedEmptyService = new EmptyService();
+		EmptyService qualifiedEmptyService = new QualifiedService();
+		registry.register(unqualifiedEmptyService);
+		registry.register(qualifiedEmptyService);
+		registry.register(QualifiedSetterService.class);
+
+		QualifiedSetterService service =
+			registry.fetch(typed(QualifiedSetterService.class));
+
+		assertThat(service)
+			.isNotNull();
+		service.assertInjected(qualifiedEmptyService);
+	}
+
+	@Qualifier
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface TestQualifier {
+		// marker
+	}
+
 	private static class EmptyService {
+		// test class
+	}
+
+	@TestQualifier
+	private static class QualifiedService extends EmptyService {
 		// test class
 	}
 
@@ -47,6 +121,30 @@ public class StandardRegistryTest {
 		void assertAnythingInjected() {
 			assertThat(dependency)
 					.isNotNull();
+		}
+
+		void assertInjected(EmptyService expected) {
+			assertThat(dependency)
+				.isSameAs(expected);
+		}
+	}
+
+	private static class QualifiedSetterService {
+		private EmptyService dependency;
+
+		void assertAnythingInjected() {
+			assertThat(dependency)
+				.isNotNull();
+		}
+
+		void assertInjected(EmptyService expected) {
+			assertThat(dependency)
+				.isSameAs(expected);
+		}
+
+		@Inject
+		void setDependency(@TestQualifier EmptyService dependency) {
+			this.dependency = dependency;
 		}
 	}
 }
