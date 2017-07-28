127ed774797fb23836a3c445ed8a519b383a4932
Paweł Płazieński <nivertius@gmail.com>
2017-07-28 11:21:01 +0200

    Javadoc for FieldQuery

--- src/main/java/org/perfectable/introspection/query/FieldQuery.java
+++ src/main/java/org/perfectable/introspection/query/FieldQuery.java
@@ -12,69 +12,151 @@ import com.google.common.collect.ImmutableList;
 
 import static java.util.Objects.requireNonNull;
 
+
+/**
+ * Iterable-like container that searches for fields in class.
+ *
+ * <p>This is straightforward method to search for class fields that have specified characteristics in provided class.
+ *
+ * <p>Instances of this class are immutable, each filtering produces new, modified instance. To obtain unrestricted
+ * query, use {@link #of}.
+ *
+ * <p>To obtain results either iterate this class with {@link #iterator} (or in enhanced-for loop) or use one of
+ * {@link #stream()}, {@link #unique()}, {@link #option()} or {@link #isPresent()}.
+ *
+ * <p>All fields present in class are searched, either declared in it directly, or inherited from parent classes.
+ *
+ * <p>Example usage, which processes all non-static, public fields in class "User" that have annotation "Inject" on
+ * them. Before passing field to the method, it is marked as {@link Field#setAccessible accessible}.
+ * <pre>
+ *     FieldQuery.of(User.class)
+ *         .requiringModifier(Modifier.PUBLIC)
+ *         .excludingModifier(Modifier.STATIC)
+ *         .annotatedBy(Inject.class)
+ *         .asAccessible()
+ *         .stream()
+ *         .forEach(this::process);
+ * </pre>
+ */
 @SuppressWarnings({
 	"DesignForExtension", // class is closed because of package-private constructor
 	"ClassDataAbstractionCoupling"
 })
 public abstract class FieldQuery extends MemberQuery<Field, FieldQuery> {
 
+	/**
+	 * This simple Null Object Pattern for this query.
+	 *
+	 * @return Query which will produce no results.
+	 */
 	public static FieldQuery empty() {
 		return Empty.INSTANCE;
 	}
 
+	/**
+	 * Queries for fields in specified class.
+	 *
+	 * @param type class to search fields in.
+	 * @return query that returns all fields in specified class.
+	 */
 	public static FieldQuery of(Class<?> type) {
 		requireNonNull(type);
 		return new Complete<>(type);
 	}
 
+	/**
+	 * Joins results of two queries.
+	 *
+	 * <p>Resulting query will return all fields returned by this query, followed by all fields
+	 * returned by provided query.
+	 *
+	 * @param other secondary query that will be joined.
+	 * @return Query that contains results from this and provided {@code other} query.
+	 */
 	public FieldQuery join(FieldQuery other) {
 		requireNonNull(other);
 		return Composite.composite(this, other);
 	}
 
+	/**
+	 * Restricts query to fields with specified name.
+	 */
 	@Override
 	public FieldQuery named(String name) {
 		requireNonNull(name);
 		return new Named(this, name);
 	}
 
+	/**
+	 * Restricts query to fields with name that matches specified pattern.
+	 */
 	@Override
 	public FieldQuery nameMatching(Pattern namePattern) {
 		requireNonNull(namePattern);
 		return new NameMatching(this, namePattern);
 	}
 
+	/**
+	 * Restricts query to fields which matches arbitrary filter.
+	 */
 	@Override
 	public FieldQuery filter(Predicate<? super Field> filter) {
 		requireNonNull(filter);
 		return new Predicated(this, filter);
 	}
 
+	/**
+	 * Restricts query to fields with type that is subtype of specified type.
+	 *
+	 * <p>In other words, this method will restrict query to fields that when {@link Field#get get} from object, will
+	 * return instances of specified type (or null).
+	 *
+	 * @param type type to restrict
+	 * @return copy of this query that will also filter by type
+	 */
 	public FieldQuery typed(Type type) {
 		return typed(TypeFilter.subtypeOf(type));
 	}
 
+	/**
+	 * Restricts query to fields which have type that matches specified arbitrary type filter.
+	 *
+	 * @param typeFilter filter of type to restrict
+	 * @return copy of this query that will also filter by type
+	 */
 	public FieldQuery typed(TypeFilter typeFilter) {
 		requireNonNull(typeFilter);
 		return new Typed(this, typeFilter);
 	}
 
+	/**
+	 * Restricts query to fields which are annotated by specified annotation filter.
+	 */
 	@Override
 	public FieldQuery annotatedWith(AnnotationFilter annotationFilter) {
 		requireNonNull(annotationFilter);
 		return new Annotated(this, annotationFilter);
 	}
 
+	/**
+	 * Restricts query to fields that have specified modifier on them.
+	 */
 	@Override
 	public FieldQuery requiringModifier(int requiredModifier) {
 		return new RequiringModifier(this, requiredModifier);
 	}
 
+	/**
+	 * Restricts query to fields that don't have specified modifier.
+	 */
 	@Override
 	public FieldQuery excludingModifier(int excludedModifier) {
 		return new ExcludingModifier(this, excludedModifier);
 	}
 
+	/**
+	 * Makes query mark field as {Field#setAccessible accessible} when iterating.
+	 */
 	@Override
 	public FieldQuery asAccessible() {
 		return new AccessibleMarking(this);
