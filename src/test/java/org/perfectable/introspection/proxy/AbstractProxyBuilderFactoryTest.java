2bbb5938c8d982f5fdcde8679d7902a2a4ec045e
Paweł Płazieński <nivertius@gmail.com>
2017-11-11 20:34:50 +0100

    Replace forwarding calls to mocks with TestHandler

--- src/test/java/org/perfectable/introspection/proxy/AbstractProxyBuilderFactoryTest.java
+++ src/test/java/org/perfectable/introspection/proxy/AbstractProxyBuilderFactoryTest.java
@@ -1,19 +1,14 @@
 package org.perfectable.introspection.proxy;
 
-import org.perfectable.introspection.MockitoExtension;
-
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import static org.mockito.Mockito.doThrow;
 
-@ExtendWith(MockitoExtension.class)
 public abstract class AbstractProxyBuilderFactoryTest {
 
 	private static final String MESSAGE_ASSUME_SUPERCLASS = "This factory does not support superclass proxies";
@@ -22,103 +17,134 @@ public abstract class AbstractProxyBuilderFactoryTest {
 	protected abstract ProxyBuilderFactory createFactory();
 
 	@Test
-	void testCheckedException(@Mock TestInterfaceChecked targetMock) throws IOException {
+	void testCheckedException() throws IOException {
 		ProxyBuilderFactory factory = createFactory();
 		ProxyBuilder<TestInterfaceChecked> proxyBuilder =
 			factory.ofInterfaces(TestInterfaceChecked.class);
-		TestInterfaceChecked proxy = proxyBuilder.instantiate(ForwardingHandler.of(targetMock));
+		TestHandler<TestInterfaceChecked> handler = TestHandler.create();
+		TestInterfaceChecked proxy = proxyBuilder.instantiate(handler);
 
 		assertThat(proxy).isInstanceOf(TestInterfaceChecked.class);
 
 		IOException thrown = new IOException();
-		doThrow(thrown).when(targetMock).firstMethod();
+		handler.expectInvocation(proxy, TestInterfaceChecked.METHOD)
+			.andThrow(thrown);
 
-		assertThatThrownBy(() -> proxy.firstMethod())
+		assertThatThrownBy(() -> proxy.checkedMethod())
 			.isSameAs(thrown);
+
+		handler.verify();
 	}
 
 	@Test
-	void testUncheckedException(@Mock TestFirstInterface targetMock) throws IOException {
+	void testUncheckedException() throws IOException {
 		ProxyBuilderFactory factory = createFactory();
 		ProxyBuilder<TestFirstInterface> proxyBuilder =
 			factory.ofInterfaces(TestFirstInterface.class);
-		TestFirstInterface proxy = proxyBuilder.instantiate(ForwardingHandler.of(targetMock));
+		TestHandler<TestFirstInterface> handler = TestHandler.create();
+		TestFirstInterface proxy = proxyBuilder.instantiate(handler);
 
 		assertThat(proxy).isInstanceOf(TestFirstInterface.class);
 
 		IllegalArgumentException thrown = new IllegalArgumentException();
-		doThrow(thrown).when(targetMock).firstMethod();
+		handler.expectInvocation(proxy, TestFirstInterface.METHOD)
+			.andThrow(thrown);
 
 		assertThatThrownBy(() -> proxy.firstMethod())
 			.isSameAs(thrown);
+
+		handler.verify();
 	}
 
 	@Test
-	void testOfInterfaces(@Mock TestFirstInterface firstMock) {
+	void testOfInterfaces() {
 		ProxyBuilderFactory factory = createFactory();
 		ProxyBuilder<TestFirstInterface> proxyBuilder =
 			factory.ofInterfaces(TestFirstInterface.class);
-		TestFirstInterface proxy = proxyBuilder.instantiate(ForwardingHandler.of(firstMock));
+		TestHandler<TestFirstInterface> handler = TestHandler.create();
+		TestFirstInterface proxy = proxyBuilder.instantiate(handler);
 
 		assertThat(proxy).isInstanceOf(TestFirstInterface.class);
 		assertThat(proxy).isNotInstanceOf(TestClass.class);
 
-		firstMock.firstMethod();
+		handler.expectInvocation(proxy, TestFirstInterface.METHOD)
+			.andReturn(null);
+
 		proxy.firstMethod();
+
+		handler.verify();
 	}
 
 	@Test
-	void testOfClass(@Mock TestClass classMock) {
+	void testOfClass() {
 		ProxyBuilderFactory factory = createFactory();
 		assumeTrue(factory.supportsFeature(ProxyBuilderFactory.Feature.SUPERCLASS), MESSAGE_ASSUME_SUPERCLASS);
 
 		ProxyBuilder<TestClass> proxyBuilder =
 			factory.ofClass(TestClass.class);
-		TestClass proxy = proxyBuilder.instantiate(ForwardingHandler.of(classMock));
+		TestHandler<TestClass> handler = TestHandler.create();
+		TestClass proxy = proxyBuilder.instantiate(handler);
 
 		assertThat(proxy).isNotInstanceOf(TestFirstInterface.class);
 		assertThat(proxy).isInstanceOf(TestClass.class);
 
-		classMock.classMethod();
+		handler.expectInvocation(proxy, TestClass.METHOD)
+			.andReturn(null);
+
 		proxy.classMethod();
+
+		handler.verify();
 	}
 
 	@Test
-	void testOfClassWithInterface(@Mock TestClassMixed mixedMock) {
+	void testOfClassWithInterface() {
 		ProxyBuilderFactory factory = createFactory();
 		assumeTrue(factory.supportsFeature(ProxyBuilderFactory.Feature.SUPERCLASS), MESSAGE_ASSUME_SUPERCLASS);
 		ProxyBuilder<TestClass> proxyBuilder =
 			factory.ofClass(TestClass.class, TestFirstInterface.class);
-		TestClass proxy = proxyBuilder.instantiate(ForwardingHandler.of(mixedMock));
+		TestHandler<TestClass> handler = TestHandler.create();
+		TestClass proxy = proxyBuilder.instantiate(handler);
 
 		assertThat(proxy).isInstanceOf(TestFirstInterface.class);
 		assertThat(proxy).isInstanceOf(TestClass.class);
 
-		mixedMock.classMethod();
+		handler.expectInvocation(proxy, TestClass.METHOD)
+			.andReturn(null);
 		proxy.classMethod();
 
-		mixedMock.firstMethod();
+		handler.expectInvocation(proxy, TestFirstInterface.METHOD)
+			.andReturn(null);
 		((TestFirstInterface) proxy).firstMethod();
+
+		handler.verify();
 	}
 
 	public interface TestInterfaceChecked {
-		void firstMethod() throws IOException;
+		Method METHOD = extractMethod(TestInterfaceChecked.class, "checkedMethod");
+
+		void checkedMethod() throws IOException;
 	}
 
 	public interface TestFirstInterface {
+		Method METHOD = extractMethod(TestFirstInterface.class, "firstMethod");
+
 		void firstMethod();
 	}
 
 	public static class TestClass {
+		public static final Method METHOD = extractMethod(TestClass.class, "classMethod");
+
 		public void classMethod() {
 			throw new AssertionError(MESSAGE_METHOD_CALLED);
 		}
 	}
 
-	public static class TestClassMixed extends TestClass implements TestFirstInterface {
-		@Override
-		public void firstMethod() {
-			throw new AssertionError(MESSAGE_METHOD_CALLED);
+	static Method extractMethod(Class<?> declaringClass, String methodName, Class<?>... argumentTypes) {
+		try {
+			return declaringClass.getDeclaredMethod(methodName, argumentTypes);
+		}
+		catch (NoSuchMethodException e) {
+			throw new AssertionError(e);
 		}
 	}
 }
---
+++ src/test/java/org/perfectable/introspection/proxy/TestHandler.java
@@ -0,0 +1,116 @@
+package org.perfectable.introspection.proxy;
+
+import org.perfectable.introspection.ObjectMethods;
+
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.Queue;
+import javax.annotation.Nullable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestHandler<T> implements InvocationHandler<T> {
+	private final Queue<Expectance> expected = new LinkedList<>();
+
+	static <T> TestHandler<T> create() {
+		return new TestHandler<>();
+	}
+
+	@Nullable
+	@Override
+	public Object handle(Invocation<T> invocation) throws Throwable {
+		return invocation.proceed((method, receiver, arguments) -> {
+			if (ObjectMethods.EQUALS.equals(method)) {
+				return receiver == arguments[0];
+			}
+			if (ObjectMethods.HASH_CODE.equals(method)) {
+				return System.identityHashCode(receiver);
+			}
+			if (ObjectMethods.TO_STRING.equals(method)) {
+				return receiver.getClass() + "@" + System.identityHashCode(receiver);
+			}
+			Expectance expectance = expected.remove();
+			return invocation.proceed(expectance);
+		});
+	}
+
+	public Expectance expectInvocation(T proxy, Method method, Object... arguments) {
+		Expectance expectance = new Expectance(proxy, method, arguments);
+		expected.add(expectance);
+		return expectance;
+	}
+
+	public void verify() {
+		assertThat(expected)
+			.as("Expectations")
+			.isEmpty();
+	}
+
+	@FunctionalInterface
+	interface InvocationResult {
+		@Nullable
+		Object resolve() throws Throwable; // SUPPRESS IllegalThrows
+	}
+
+	private static class ReturningResult implements InvocationResult {
+		@Nullable
+		private final Object value;
+
+		ReturningResult(@Nullable Object value) {
+			this.value = value;
+		}
+
+		@Nullable
+		@Override
+		public Object resolve() throws Throwable {
+			return value;
+		}
+	}
+
+	private static class ThrowingResult implements InvocationResult {
+		private final Throwable thrown;
+
+		ThrowingResult(Throwable thrown) {
+			this.thrown = thrown;
+		}
+
+		@Nullable
+		@Override
+		public Object resolve() throws Throwable {
+			throw thrown;
+		}
+	}
+
+	class Expectance implements Invocation.Invoker<T> {
+		private final T expectedProxy;
+		private final Method expectedMethod;
+		private final Object[] expectedArguments;
+
+		private InvocationResult result =
+			() -> new AssertionError("Result has not been set");
+
+		Expectance(T expectedProxy, Method expectedMethod, Object... expectedArguments) {
+			this.expectedProxy = expectedProxy;
+			this.expectedMethod = expectedMethod;
+			this.expectedArguments = expectedArguments.clone();
+		}
+
+		public void andReturn(@Nullable Object value) {
+			this.result = new ReturningResult(value);
+		}
+
+		public void andThrow(Throwable thrown) {
+			this.result = new ThrowingResult(thrown);
+		}
+
+		@Nullable
+		@Override
+		public Object process(Method method, @Nullable T receiver, Object... arguments) throws Throwable {
+			assertThat(receiver).isEqualTo(expectedProxy);
+			assertThat(method).isEqualTo(expectedMethod);
+			assertThat(arguments).isEqualTo(expectedArguments);
+			return result.resolve();
+		}
+	}
+
+}
