8b818d346fc0ed5ad6594ab50f6b8b03bcb86a40
Paweł Płazieński <nivertius@gmail.com>
2018-02-25 18:47:15 +0100

    Use custom assert for type filter

---
+++ src/test/java/org/perfectable/introspection/query/TypeFilterAssert.java
@@ -0,0 +1,30 @@
+package org.perfectable.introspection.query;
+
+import org.assertj.core.api.AbstractObjectAssert;
+
+final class TypeFilterAssert extends AbstractObjectAssert<TypeFilterAssert, TypeFilter> {
+
+	private TypeFilterAssert(TypeFilter typeFilter) {
+		super(typeFilter, TypeFilterAssert.class);
+	}
+
+	static TypeFilterAssert assertThat(TypeFilter typeFilter) {
+		return new TypeFilterAssert(typeFilter);
+	}
+
+	TypeFilterAssert matchesType(Class<?> testedClass) {
+		isNotNull();
+		if (!actual.matches(testedClass)) {
+			failWithMessage("Expected filter to match class <%s>", testedClass.getName());
+		}
+		return myself;
+	}
+
+	TypeFilterAssert doesntMatchType(Class<?> testedClass) {
+		isNotNull();
+		if (actual.matches(testedClass)) {
+			failWithMessage("Expected filter not to match class <%s>", testedClass.getName());
+		}
+		return myself;
+	}
+}
--- src/test/java/org/perfectable/introspection/query/TypeFilterTest.java
+++ src/test/java/org/perfectable/introspection/query/TypeFilterTest.java
@@ -4,238 +4,260 @@ import java.io.Serializable;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.perfectable.introspection.query.TypeFilterAssert.assertThat;
 
 // SUPPRESS NEXT 1 MethodCount
 class TypeFilterTest {
+
 	@Test
 	void all() {
 		TypeFilter filter = TypeFilter.ALL;
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isTrue();
-		assertThat(filter.matches(Serializable.class)).isTrue();
-		assertThat(filter.matches(Long.class)).isTrue();
-		assertThat(filter.matches(String.class)).isTrue();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.matchesType(Object.class)
+			.matchesType(Serializable.class)
+			.matchesType(Long.class)
+			.matchesType(String.class);
 	}
 
 	@Test
 	void none() {
 		TypeFilter filter = TypeFilter.NONE;
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void noneWithExcluded() {
 		TypeFilter filter = TypeFilter.NONE.withExcluded(Number.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void noneWithLowerBound() {
 		TypeFilter filter = TypeFilter.NONE.withLowerBound(Number.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void noneWithUpperBound() {
 		TypeFilter filter = TypeFilter.NONE.withUpperBound(Number.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void withUpperBound() {
 		TypeFilter filter = TypeFilter.ALL.withUpperBound(Number.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isTrue();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.matchesType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void withLowerBound() {
 		TypeFilter filter = TypeFilter.ALL.withLowerBound(Number.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isTrue();
-		assertThat(filter.matches(Serializable.class)).isTrue();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.matchesType(Object.class)
+			.matchesType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void withExcluded() {
 		TypeFilter filter = TypeFilter.ALL.withExcluded(Number.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isTrue();
-		assertThat(filter.matches(Serializable.class)).isTrue();
-		assertThat(filter.matches(Long.class)).isTrue();
-		assertThat(filter.matches(String.class)).isTrue();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.matchesType(Object.class)
+			.matchesType(Serializable.class)
+			.matchesType(Long.class)
+			.matchesType(String.class);
 	}
 
 	@Test
 	void withExcludedTwiceSame() {
 		TypeFilter filter = TypeFilter.ALL.withExcluded(Number.class).withExcluded(Number.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isTrue();
-		assertThat(filter.matches(Serializable.class)).isTrue();
-		assertThat(filter.matches(Long.class)).isTrue();
-		assertThat(filter.matches(String.class)).isTrue();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.matchesType(Object.class)
+			.matchesType(Serializable.class)
+			.matchesType(Long.class)
+			.matchesType(String.class);
 	}
 
 	@Test
 	void withExcludedTwiceDifferent() {
 		TypeFilter filter = TypeFilter.ALL.withExcluded(Number.class).withExcluded(Serializable.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isTrue();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isTrue();
-		assertThat(filter.matches(String.class)).isTrue();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.matchesType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.matchesType(Long.class)
+			.matchesType(String.class);
 	}
 
 	@Test
 	void superTypeOf() {
 		TypeFilter filter = TypeFilter.superTypeOf(Number.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isTrue();
-		assertThat(filter.matches(Serializable.class)).isTrue();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.matchesType(Object.class)
+			.matchesType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void subtypeOf() {
 		TypeFilter filter = TypeFilter.subtypeOf(Number.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isTrue();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.matchesType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exact() {
 		TypeFilter filter = TypeFilter.exact(Number.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithExcludedDifferent() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withExcluded(String.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithExcludedMatching() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withExcluded(Number.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithUpperBoundMatching() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withUpperBound(Object.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithUpperBoundMismatchingSubtype() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withUpperBound(Long.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithUpperBoundMismatchingDifferent() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withUpperBound(String.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithLowerBoundMatching() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withLowerBound(Long.class);
 
-		assertThat(filter.matches(Number.class)).isTrue();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.matchesType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithLowerBoundMismatchingSupertype() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withLowerBound(Object.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 
 	@Test
 	void exactWithLowerBoundMismatchingDifferent() {
 		TypeFilter filter = TypeFilter.exact(Number.class).withLowerBound(String.class);
 
-		assertThat(filter.matches(Number.class)).isFalse();
-		assertThat(filter.matches(Object.class)).isFalse();
-		assertThat(filter.matches(Serializable.class)).isFalse();
-		assertThat(filter.matches(Long.class)).isFalse();
-		assertThat(filter.matches(String.class)).isFalse();
+		assertThat(filter)
+			.doesntMatchType(Number.class)
+			.doesntMatchType(Object.class)
+			.doesntMatchType(Serializable.class)
+			.doesntMatchType(Long.class)
+			.doesntMatchType(String.class);
 	}
 }
