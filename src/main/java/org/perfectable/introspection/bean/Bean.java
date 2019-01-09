ce8c1137faf1e53c33d2233d1458438ca9961ee0
Paweł Płazieński <nivertius@gmail.com>
2019-01-09 21:26:19 +0100

    Javadoc for Bean

--- src/main/java/org/perfectable/introspection/bean/Bean.java
+++ src/main/java/org/perfectable/introspection/bean/Bean.java
@@ -9,6 +9,17 @@ import com.google.common.collect.ImmutableSet;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Represents a view of an instance as its property values, typically containing a proper Java Bean.
+ *
+ * <p>Instances are created using {@link #from}.
+ *
+ * <p>Allows access to properties either by name (using {@link #property}) or by listing them
+ * (eg. {@link #fieldProperties}).
+ * Properties are defined as either getter and/or setter or field.
+ *
+ * @param <B> type of instance contained in this bean
+ */
 public final class Bean<B> {
 
 	private final B instance;
@@ -17,24 +28,55 @@ public final class Bean<B> {
 		this.instance = instance;
 	}
 
+	/**
+	 * Creates bean from Java Bean object.
+	 *
+	 * @param instance a non-null object that this bean will allow introspection into
+	 * @param <X> Instance type
+	 * @return Bean containing specified instance
+	 */
 	public static <X> Bean<X> from(X instance) {
 		requireNonNull(instance);
 		return new Bean<>(instance);
 	}
 
+	/**
+	 * Extracts instance that is backing this bean.
+	 *
+	 * @return instance that is backing this bean.
+	 */
 	public B contents() {
 		return instance;
 	}
 
+	/**
+	 * Extracts actual type of backing instance.
+	 *
+	 * @return actual type of backing instance.
+	 */
 	@SuppressWarnings("unchecked")
 	public Class<B> type() {
 		return (Class<B>) this.instance.getClass();
 	}
 
+	/**
+	 * Finds property by name.
+	 *
+	 * @param name name of the property to look for.
+	 * @return property with provided name
+	 * @throws IllegalArgumentException when there is no property with provided name
+	 */
 	public Property<B, Object> property(String name) {
 		return Properties.create(type(), name).bind(this.instance);
 	}
 
+	/**
+	 * Creates new bean with contained instance copied field-by-field from this bean backing instance.
+	 *
+	 * <p>Copy is created by default constructor.
+	 *
+	 * @return copy of this bean.
+	 */
 	public Bean<B> copy() {
 		Bean<B> duplicate = BeanSchema.from(type()).instantiate();
 		fieldProperties()
@@ -42,6 +84,11 @@ public final class Bean<B> {
 		return duplicate;
 	}
 
+	/**
+	 * Lists all properties that are backed by a field.
+	 *
+	 * @return all properties backed by a field.
+	 */
 	public ImmutableSet<Property<B, ?>> fieldProperties() {
 		return FieldQuery.of(type())
 			.excludingModifier(Modifier.STATIC)
@@ -50,6 +97,11 @@ public final class Bean<B> {
 			.collect(ImmutableSet.toImmutableSet());
 	}
 
+	/**
+	 * Finds all object that backing instance has reference to.
+	 *
+	 * @return all objects that backing instance has reference to.
+	 */
 	public ImmutableSet<Object> related() {
 		return this.fieldProperties().stream()
 				.map(Property::get)
