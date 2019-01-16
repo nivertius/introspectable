09c640a89ebec2201a04db241c3feb42aa98733d
Paweł Płazieński <nivertius@gmail.com>
2019-01-16 21:39:42 +0100

    Javadoc for ClassLoaderIntrospection

--- src/main/java/org/perfectable/introspection/ClassLoaderIntrospection.java
+++ src/main/java/org/perfectable/introspection/ClassLoaderIntrospection.java
@@ -2,21 +2,36 @@ package org.perfectable.introspection;
 
 import org.perfectable.introspection.query.ClassQuery;
 
+/**
+ * Entry point for {@link ClassLoader} introspections.
+ *
+ * <p>Use {@link Introspections#introspect(ClassLoader)} to get instance of this class.
+ */
 public final class ClassLoaderIntrospection {
-	private final ClassLoader classLoader;
-
-	private ClassLoaderIntrospection(ClassLoader classLoader) {
-		this.classLoader = classLoader;
-	}
-
-	public static ClassLoaderIntrospection of(ClassLoader classLoader) {
+	static ClassLoaderIntrospection of(ClassLoader classLoader) {
 		return new ClassLoaderIntrospection(classLoader);
 	}
 
+	/**
+	 * Query for classes that introspected classloader provides.
+	 *
+	 * @return query for classes in classloader
+	 */
 	public ClassQuery<Object> classes() {
 		return ClassQuery.of(classLoader);
 	}
 
+	/**
+	 * Loads class assuming that it exists in classloader.
+	 *
+	 * <p>This asserts that class name is valid and classloader is able to load class with this name.
+	 *
+	 * <p>This method can be compared to {@link ClassLoader#loadClass(String)}, but with no compile-time exceptions.
+	 *
+	 * @param className fully qualified class name of class to load
+	 * @return loaded class
+	 * @throws AssertionError when class loading actually fails.
+	 */
 	public Class<?> loadSafe(String className) {
 		try {
 			return classLoader.loadClass(className);
@@ -24,5 +39,11 @@ public final class ClassLoaderIntrospection {
 		catch (ClassNotFoundException e) {
 			throw new AssertionError(e);
 		}
+	}
+
+	private final ClassLoader classLoader;
+
+	private ClassLoaderIntrospection(ClassLoader classLoader) {
+		this.classLoader = classLoader;
 	}
 }
