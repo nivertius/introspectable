f1033d07831737eac0ed85588f36a7e67baaa3bc
Paweł Płazieński <nivertius@gmail.com>
2019-01-16 21:32:39 +0100

    Javadoc for Introspections

--- src/main/java/org/perfectable/introspection/Introspections.java
+++ src/main/java/org/perfectable/introspection/Introspections.java
@@ -3,20 +3,50 @@ package org.perfectable.introspection;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+/**
+ * Convenient entry point for introspections.
+ *
+ * <p>Use {@link #introspect} to start introspection for either supported type.
+ */
 public final class Introspections {
 
+	/**
+	 * Introspect a class.
+	 *
+	 * @param type class to introspect
+	 * @param <X> class parameter
+	 * @return class introspections
+	 */
 	public static <X> ClassIntrospection<X> introspect(Class<X> type) {
 		return ClassIntrospection.of(type);
 	}
 
+	/**
+	 * Introspect a method.
+	 *
+	 * @param method method to introspect
+	 * @return method introspections
+	 */
 	public static MethodIntrospection introspect(Method method) {
 		return MethodIntrospection.of(method);
 	}
 
+	/**
+	 * Introspect a field.
+	 *
+	 * @param field field to introspect
+	 * @return field introspections
+	 */
 	public static FieldIntrospection introspect(Field field) {
 		return FieldIntrospection.of(field);
 	}
 
+	/**
+	 * Introspect a classloader.
+	 *
+	 * @param classLoader class loader to introspect
+	 * @return classloader introspections
+	 */
 	public static ClassLoaderIntrospection introspect(ClassLoader classLoader) {
 		return ClassLoaderIntrospection.of(classLoader);
 	}
