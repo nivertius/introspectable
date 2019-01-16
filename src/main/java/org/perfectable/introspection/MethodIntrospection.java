d74a4f400404914ca1120754a7a95d5b66bc490f
Paweł Płazieński <nivertius@gmail.com>
2019-01-16 21:35:07 +0100

    Javadoc for MethodIntrospection

--- src/main/java/org/perfectable/introspection/MethodIntrospection.java
+++ src/main/java/org/perfectable/introspection/MethodIntrospection.java
@@ -6,20 +6,37 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+/**
+ * Entry point for method introspections.
+ *
+ * <p>Use {@link Introspections#introspect(Method)} to get instance of this class.
+ */
 public final class MethodIntrospection {
 	static MethodIntrospection of(Method method) {
 		return new MethodIntrospection(method);
 	}
 
+	/**
+	 * Query for annotations on specified method.
+	 *
+	 * @return annotations query on introspected method.
+	 */
 	public AnnotationQuery<Annotation> annotations() {
 		return AnnotationQuery.of(method);
 	}
 
-	private final Method method;
-
+	/**
+	 * Check if method is callable.
+	 *
+	 * <p>This only checks there is a scenario that introspected method can be called.
+	 *
+	 * @return if method is callable.
+	 */
 	public boolean isCallable() {
 		return !Modifier.isAbstract(method.getModifiers());
 	}
+
+	private final Method method;
 
 	private MethodIntrospection(Method method) {
 		this.method = method;
