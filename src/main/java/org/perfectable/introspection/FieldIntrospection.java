e82a3a68c083e137ebf8e0e1aaf20caa00d90994
Paweł Płazieński <nivertius@gmail.com>
2019-01-16 21:55:09 +0100

    Javadoc for FieldIntrospections

--- src/main/java/org/perfectable/introspection/FieldIntrospection.java
+++ src/main/java/org/perfectable/introspection/FieldIntrospection.java
@@ -5,11 +5,21 @@ import org.perfectable.introspection.query.AnnotationQuery;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
+/**
+ * Entry point for {@link Field} introspections.
+ *
+ * <p>Use {@link Introspections#introspect(Field)} to get instance of this class.
+ */
 public final class FieldIntrospection {
 	static FieldIntrospection of(Field field) {
 		return new FieldIntrospection(field);
 	}
 
+	/**
+	 * Query for annotations on introspected field.
+	 *
+	 * @return query for annotations
+	 */
 	public AnnotationQuery<Annotation> annotations() {
 		return AnnotationQuery.of(field);
 	}
