24dd7192e9bd852f022b9ac05d0b1b72ab26205c
Paweł Płazieński <nivertius@gmail.com>
2019-01-21 18:55:23 +0100

    Make JdkProxyBuilderTest fail assumtions on proxying classes

--- src/test/java/org/perfectable/introspection/proxy/jdk/JdkProxyBuilderTest.java
+++ src/test/java/org/perfectable/introspection/proxy/jdk/JdkProxyBuilderTest.java
@@ -3,21 +3,9 @@ package org.perfectable.introspection.proxy.jdk;
 import org.perfectable.introspection.proxy.AbstractProxyBuilderFactoryTest;
 import org.perfectable.introspection.proxy.ProxyBuilderFactory;
 
-import org.junit.jupiter.api.Test;
-
-class JdkProxyBuilderTest extends AbstractProxyBuilderFactoryTest {
+class JdkProxyBuilderTest extends AbstractProxyBuilderFactoryTest { // SUPPRESS TestClassWithoutTestCases
 	@Override
 	protected ProxyBuilderFactory createFactory() {
 		return new JdkProxyBuilderFactory();
-	}
-
-	@Test
-	void testOfClass() {
-		// test assumption will fail
-	}
-
-	@Test
-	void testOfClassWithInterface() {
-		// test assumption will fail
 	}
 }
