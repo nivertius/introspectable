c746728281e6e742519d3140b5288a1a4f5238c3
Paweł Płazieński <nivertius@gmail.com>
2020-04-10 22:33:51 +0200

    Javadoc for InvocationHandler

--- src/main/java/org/perfectable/introspection/proxy/InvocationHandler.java
+++ src/main/java/org/perfectable/introspection/proxy/InvocationHandler.java
@@ -2,10 +2,27 @@ package org.perfectable.introspection.proxy;
 
 import javax.annotation.Nullable;
 
+/**
+ * Handles an invocation of a execution point on a proxy and returns response.
+ *
+ * <p>This is main interface to be implemented when creating proxies. Every call on the proxy will be passed to
+ * {@link #handle}, and result of this method will be returned by proxy.
+ *
+ * @param <I> type of invocation supported. This is usually {@link MethodInvocation}
+
+ * @see ProxyBuilder#instantiate
+ * @see MethodInvocation
+ */
 @FunctionalInterface
 public interface InvocationHandler<I extends Invocation> {
 
-	// SUPPRESS NEXT 2 IllegalThrows generic exception is actually thrown
+	/**
+	 * Catches invocation executed on proxy and returns result that should be passed to the client.
+	 *
+	 * @param invocation invocation captured
+	 * @return successful result of invocation processing
+	 * @throws Throwable exception that will be thrown on the call site
+	 */
 	@Nullable
-	Object handle(I invocation) throws Throwable;
+	Object handle(I invocation) throws Throwable; // SUPPRESS IllegalThrows
 }
