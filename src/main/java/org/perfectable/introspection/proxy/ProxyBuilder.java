331f80e78ca1de6961fc45cf6434536919cae1f8
Paweł Płazieński <nivertius@gmail.com>
2020-04-13 19:00:38 +0200

    Javadoc for ProxyBuilder and ProxyService

--- src/main/java/org/perfectable/introspection/proxy/ProxyBuilder.java
+++ src/main/java/org/perfectable/introspection/proxy/ProxyBuilder.java
@@ -7,6 +7,15 @@ import com.google.errorprone.annotations.Immutable;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+/**
+ * Builder pattern for proxies.
+ *
+ * <p>This class allows chained and intuitive creation of proxy instances. It uses {@link ProxyService} to actually
+ * create proxies, and loads required service automatically.
+ *
+ * @see ProxyService
+ * @param <I> type of proxies built
+ */
 @Immutable
 public final class ProxyBuilder<I> {
 	private ProxyBuilder(ClassLoader classLoader, Class<?> baseClass,
@@ -23,6 +32,17 @@ public final class ProxyBuilder<I> {
 	private final ImmutableList<? extends Class<?>> interfaces;
 	private final ProxyService service;
 
+	/**
+	 * Creates new builder for class that is actually a class (not an interface).
+	 *
+	 * <p>This class will be used as a superclass of a proxy class. The class must not be final or primitive.
+	 *
+	 * <p>>This builder will use service that supports building proxies with superclasses.
+	 *
+	 * @param superclass class to be used as superclass for a proxy
+	 * @param <X> type of proxy
+	 * @return new proxy builder with specified class
+	 */
 	public static <X> ProxyBuilder<X> forClass(Class<X> superclass) {
 		checkArgument(!superclass.isInterface());
 		checkArgument(!superclass.isPrimitive());
@@ -32,6 +52,16 @@ public final class ProxyBuilder<I> {
 		return new ProxyBuilder<X>(classLoader, superclass, ImmutableList.of(), service);
 	}
 
+	/**
+	 * Creates new builder for interface.
+	 *
+	 * <p>This interface will be implemented by proxy class. Proxy class will not have any specific superclass, it will
+	 * probably be {@link Object}.
+	 *
+	 * @param baseInterface interface that will be implemented by proxy class
+	 * @param <X> type of proxy
+	 * @return new proxy builder with specified interface
+	 */
 	public static <X> ProxyBuilder<X> forInterface(Class<X> baseInterface) {
 		checkArgument(baseInterface.isInterface());
 		ClassLoader classLoader = baseInterface.getClassLoader();
@@ -39,6 +69,16 @@ public final class ProxyBuilder<I> {
 		return new ProxyBuilder<>(classLoader, Object.class, ImmutableList.of(baseInterface), service);
 	}
 
+	/**
+	 * Creates new builder for specified type.
+	 *
+	 * <p>Depending on if {@code resultClass} is an interface or not, this method will behave either as
+	 * {@link #forInterface} or {@link #forClass}
+	 *
+	 * @param resultClass base proxy type
+	 * @param <X> type of proxy
+	 * @return new proxy builder with specified interface
+	 */
 	public static <X> ProxyBuilder<X> forType(Class<X> resultClass) {
 		if (resultClass.isInterface()) {
 			return forInterface(resultClass);
@@ -46,6 +86,12 @@ public final class ProxyBuilder<I> {
 		return forClass(resultClass);
 	}
 
+	/**
+	 * Creates proxy builder that have additional interface added to the proxy class.
+	 *
+	 * @param additionalInterface added interface
+	 * @return new proxy builder with additional interface
+	 */
 	public ProxyBuilder<I> withInterface(Class<?> additionalInterface) {
 		checkArgument(additionalInterface.isInterface());
 		checkArgument(classLoader.equals(additionalInterface.getClassLoader()));
@@ -54,10 +100,25 @@ public final class ProxyBuilder<I> {
 		return new ProxyBuilder<>(classLoader, baseClass, newInterfaces, service);
 	}
 
+	/**
+	 * Creates proxy builder that have proxy service replaced.
+	 *
+	 * <p>This method is optional, and needed only if specific service needs to be used. ProxyBuilder will normally
+	 * select suitable service.
+	 *
+	 * @param newService proxy service to use for creating services
+	 * @return new proxy builder with replaced service
+	 */
 	public ProxyBuilder<I> usingService(ProxyService newService) {
 		return new ProxyBuilder<>(classLoader, baseClass, interfaces, newService);
 	}
 
+	/**
+	 * Creates instance of configured proxy class.
+	 *
+	 * @param handler method that proxy will delegate its calls to
+	 * @return proxy instance
+	 */
 	public I instantiate(InvocationHandler<? super MethodInvocation<I>> handler) {
 		return service.instantiate(classLoader, baseClass, interfaces, handler);
 	}
--- src/main/java/org/perfectable/introspection/proxy/ProxyService.java
+++ src/main/java/org/perfectable/introspection/proxy/ProxyService.java
@@ -7,35 +7,107 @@ import java.util.stream.Stream;
 
 import com.google.errorprone.annotations.Immutable;
 
+/**
+ * Method of producing proxies.
+ *
+ * <p>There are multiple ways of creating proxy in Java, from simple {@link java.lang.reflect.Proxy}, through support
+ * frameworks like javassist, to class generation libraries like cglib or bytebuddy. This class creates universal facade
+ * to potentially all these methods.
+ *
+ * <p>Different frameworks supports different proxying features, for example, javassist can create proxy classes that
+ * extend specific non-final class, not only that implement specific interfaces. These features are represented by
+ * {@link Feature} and can be tested for support (using {@link #supportsAllFeatures} or {@link #supportsFeature}).
+ *
+ * <p>Interface implementation can be registered to be used with {@link ServiceLoader}, and should be obtained using
+ * {@link #INSTANCES}.
+ *
+ * <p>This interface can be implemented externally, and registered with Service Loader to be usable in
+ * {@link ProxyBuilder}.
+ *
+ * @see ProxyBuilder
+ */
 @Immutable
 public interface ProxyService {
-
+	/**
+	 * Container managing all instances registered with {@link ServiceLoader}.
+	 */
 	Container INSTANCES = new Container();
 
+	/**
+	 * If the concrete service supports specified feature.
+	 *
+	 * @param feature feature to test
+	 * @return if service supports feature
+	 */
 	boolean supportsFeature(Feature feature);
 
+	/**
+	 * If the concrete service supports all of specified features.
+	 *
+	 * @param features feature set to test
+	 * @return if service supports features
+	 */
 	default boolean supportsAllFeatures(Feature... features) {
 		return Stream.of(features).allMatch(this::supportsFeature);
 	}
 
+	/**
+	 * Creates proxy instance.
+	 *
+	 * <p>This method either creates new proxy class, or reuses one that matches. This proxy class extends
+	 * {@code baseClass} and implements {@code interfaces}, and is created in {@code classLoader}. After creating this
+	 * class, its object is instantiated. The object will delegate every call that it receives to specified
+	 * {@link InvocationHandler}. The only exception is {@link Object#finalize}, which must be ignored.
+	 *
+	 * <p>This method assumes that the arguments were correctly prepared:
+	 * <ul>
+	 *     <li>{@code baseClass} is not final</li>
+	 *     <li>{@code baseClass} is actually a class (not an interface)</li>
+	 *     <li>{@code baseClass} and each element of {@code interfaces} has classloader exactly {@code classLoader}</li>
+	 *     <li>Each element in {@code interfaces} is an interface (not a class)</li>
+	 * </ul>
+	 *
+	 * @param <I> type of proxy
+	 * @param classLoader classloader used to create proxy class
+	 * @param baseClass proxy superclass, cannot be final, must be a class
+	 * @param interfaces additional interfaces that proxy class should implement
+	 * @param handler invocation handler to pass method execution for this proxy instance
+	 * @return proxy instance
+	 * @throws UnsupportedFeatureException when this service cannot create proxy with requested parameters, because
+	 *     it doesn't support some feature required to do so
+	 */
 	<I> I instantiate(ClassLoader classLoader, Class<?> baseClass, List<? extends Class<?>> interfaces,
 							  InvocationHandler<? super MethodInvocation<I>> handler)
 		throws UnsupportedFeatureException;
 
+	/** Thrown when factory cannot create a builder with requested features. */
 	@SuppressWarnings("serial")
 	final class UnsupportedFeatureException extends RuntimeException {
+		@SuppressWarnings("javadoc")
 		public UnsupportedFeatureException(String message) {
 			super(message);
 		}
 	}
 
+	/** Features that service can support. */
 	enum Feature {
+		/** Service can create proxies that have superclass other than Object. */
 		SUPERCLASS
 	}
 
+	/**
+	 * Container that manages service-loaded instances of {@link ProxyService}.
+	 */
 	final class Container {
 		private final ServiceLoader<ProxyService> cache = ServiceLoader.load(ProxyService.class);
 
+		/**
+		 * Extracts proxy service with specified features.
+		 *
+		 * @param requiredFeatures features that returned service must support
+		 * @return proxy service
+		 * @throws UnsupportedFeatureException when no proxy service could be found with specified features.
+		 */
 		public ProxyService get(Feature... requiredFeatures) {
 			for (ProxyService service : cache) {
 				if (service.supportsAllFeatures(requiredFeatures)) {
