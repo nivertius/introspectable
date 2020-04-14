4ec05acf5b9b2e3e37cbfd24670eb9fc0d6937d0
Paweł Płazieński <nivertius@gmail.com>
2020-04-15 00:10:31 +0200

    Javadoc for AnnotationQuery

--- src/main/java/org/perfectable/introspection/query/AnnotationQuery.java
+++ src/main/java/org/perfectable/introspection/query/AnnotationQuery.java
@@ -20,28 +20,88 @@ import com.google.common.collect.ImmutableSet;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Iterable-like container that finds annotations.
+ *
+ * <p>Instances of this class are immutable, each filtering produces new, modified instance. To obtain query for
+ * specific class, use {@link #of}.
+ *
+ * <p>To obtain results either iterate this class with {@link #iterator} (or in enhanced-for loop) or use one of
+ * {@link #stream()}, {@link #unique()}, {@link #option()} or {@link #isPresent()}.
+ *
+ * <p>This query handles repeatable annotations on elements as if they would be placed in a container annotation,
+ * exactly as it is written in bytecode. To get behavior that was introduced in Java 1.8 in
+ * {@link AnnotatedElement#getDeclaredAnnotationsByType}, this query needs manual expansion of container annotations.
+ * See {@link #withRepeatableUnroll} for details.
+ *
+ * <p>Example usage, which collects all annotations on {@code singletonClass} that are meta-annotated by Qualifier.
+ * <pre>
+ *     AnnotationQuery.of(singletonClass)
+ * 			.annotatedWith(javax.inject.Qualifier.class)
+ * 			.stream()
+ * 			.collect(ImmutableSet.toImmutableSet());
+ * </pre>
+ *
+ * @param <A> base annotation type
+ */
 @SuppressWarnings({
 	"DesignForExtension" // class is closed because of package-private constructor
 })
 public abstract class AnnotationQuery<A extends Annotation>
 		extends AbstractQuery<A, AnnotationQuery<A>> {
-
+	/**
+	 * This simple Null Object Pattern for this query.
+	 *
+	 * @return Query which will produce no results.
+	 */
 	public static AnnotationQuery<Annotation> empty() {
 		return Empty.INSTANCE;
 	}
 
+	/**
+	 * Creates a query from annotations declared on specified element.
+	 *
+	 * <p>Only annotations with {@link java.lang.annotation.RetentionPolicy#RUNTIME} will be returned.
+	 *
+	 * @param element element to extract annotations from
+	 * @return annotation query that will list annotations
+	 */
 	public static AnnotationQuery<Annotation> of(AnnotatedElement element) {
 		return new OfElement(element);
 	}
 
+	/**
+	 * Converts set of annotations to query.
+	 *
+	 * <p>Sets stream can be used to filter and does essentially the same, but this query allows more expressive filters
+	 * used on specified set.
+	 *
+	 * @param set set to search on
+	 * @return annotation query that will list exactly the set contents
+	 */
 	public static AnnotationQuery<Annotation> fromElements(Set<Annotation> set) {
 		return new OfSet(ImmutableSet.copyOf(set));
 	}
 
-	public static AnnotationQuery<Annotation> fromElements(Annotation... candidates) {
-		return fromElements(ImmutableSet.copyOf(candidates));
+	/**
+	 * Converts array of annotations to query.
+	 *
+	 * <p>Stream of array contents can be used to filter and does essentially the same, but this query allows more
+	 * expressive filters used.
+	 *
+	 * @param elements annotations to search from to search on
+	 * @return annotation query that will list exactly the array contents
+	 */
+	public static AnnotationQuery<Annotation> fromElements(Annotation... elements) {
+		return fromElements(ImmutableSet.copyOf(elements));
 	}
 
+	/**
+	 * Creates query that contains results from both this and provided query.
+	 *
+	 * @param other annotation query to add
+	 * @return annotation query that will list results of this and then the other query
+	 */
 	@SuppressWarnings("unchecked")
 	public AnnotationQuery<Annotation> join(AnnotationQuery<?> other) {
 		return Composite.composite(this, other);
@@ -53,16 +113,49 @@ public abstract class AnnotationQuery<A extends Annotation>
 		return new Predicated<>(this, filter);
 	}
 
+	/**
+	 * Creates query that filters resulting annotations that have meta-annotation placed on them.
+	 *
+	 * @param metaAnnotation annotation that must be placed on resulting annotation to be returned by query
+	 * @return annotation query that will list results that have specific annotations placed on them
+	 */
 	public AnnotationQuery<A> annotatedWith(Class<? extends Annotation> metaAnnotation) {
 		requireNonNull(metaAnnotation);
 		return new Annotated<>(this, metaAnnotation);
 	}
 
+	/**.
+	 * Creates query that filters annotation that have specific type as a supertype.
+	 *
+	 * @param annotationClass annotation type to filters
+	 * @param <X> new query result type
+	 * @return annotation query that will list results that are of specific type
+	 */
 	public <X extends Annotation> AnnotationQuery<X> typed(Class<X> annotationClass) {
 		requireNonNull(annotationClass);
 		return new Typed<>(this, annotationClass);
 	}
 
+	/**
+	 * Expands repeatable annotations for the results of returned query.
+	 *
+	 * <p>Normally, query handles repeatable annotations on elements as if they would be placed in a container
+	 * annotation, exactly as it is written in bytecode. This method will add that repeatable annotations to the
+	 * container.
+	 *
+	 * <p>This method changes results, so that every annotation, that would be returned, if its a
+	 * containing annotation, annotations that are embedded in it are also returned as a results. Containing
+	 * annotation is also returned, if it matches other filters. See JLS 9.6.3.
+	 *
+	 * <p>Unfortunately, repeatable annotation extraction in native Java is done with special care that is unavailable
+	 * for public, for example in OpenJDK, the sun.reflect.annotation.AnnotationType is used, which is contained in a
+	 * Class instance of an annotation type. Both field containing and the type is not public. Therefore this
+	 * query tries to reconstruct relationship between containing and contained types. This has some initial performance
+	 * penalty, as "value" method must be reflected. This class caches the information, so successive calls should
+	 * be faster.	 *
+	 *
+	 * @return annotation query that will list repeatable annotations extracted from containers
+	 */
 	public AnnotationQuery<Annotation> withRepeatableUnroll() {
 		return new RepeatableUnroll(this);
 	}
